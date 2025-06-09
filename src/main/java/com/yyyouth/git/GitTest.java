36bd257cd323405c7c1150741257fdc0efea2480
yyyouthzg <3301904493@qq.com>
2025-06-09 22:24:28 +0800

    第一次用idea提交到本地仓库

---
+++ src/main/java/com/yyyouth/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yyyouth.git;
+
+/**
+ * @Author:忧郁的青年
+ * @Date:2025/06/09
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
